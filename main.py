07c3745d34c7bb07deb289c27791a168a6d19489
Your Name <you@example.com>
2025-10-30 10:20:53 +0700

    Add login API

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import uvicorn
 from fastapi import FastAPI
 
 app = FastAPI()
@@ -12,3 +13,6 @@ async def root():
 async def say_hello(name: str):
     return {"message": f"Hello {name}"}
 
+@app.get("/alo/{name")
+async def say_alo(name: str):
+    return {"message": f"Hello {name}"}
