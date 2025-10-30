03d5f6264da312e82f78ad1228dfb319bed2a2ab
Your Name <you@example.com>
2025-10-30 11:10:05 +0700

    feature/tesing

---
+++ test/test_main.py
@@ -0,0 +1,15 @@
+from fastapi.testclient import TestClient
+from main import app
+
+client = TestClient(app)
+
+def test_root():
+    response = client.get("/")
+    assert response.status_code == 200
+    assert response.json() == {"message": "Hello World"}
+
+def test_say_hello():
+    name = "Dương"
+    response = client.get(f"/hello/{name}")
+    assert response.status_code == 200
+    assert response.json() == {"message": f"Hello {name}"}
